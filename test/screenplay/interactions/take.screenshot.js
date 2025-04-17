06761b3404689b54e3f1252d562a2ed899a718b7
Daniel Webb <daniel.webb@esynergy.co.uk>
2025-04-17 11:00:16 +0100

    add screenshot capability

---
+++ test/screenplay/interactions/take.screenshot.js
@@ -0,0 +1,14 @@
+export default class TakeScreenshot {
+  static named(name) {
+    return new TakeScreenshot(name)
+  }
+
+  constructor(name) {
+    this.name = name || 'Screenshot'
+  }
+
+  async performAs(actor) {
+    await actor.ability.takeScreenshot(this.name)
+    return true
+  }
+}
