a0206dc53f2d935f9766d5032cd251db430dad97
Daniel Webb <daniel.webb@esynergy.co.uk>
2025-05-13 16:43:29 +0100

    add json when actor memory is changed to the report

--- test-infrastructure/screenplay/actor.js
+++ test-infrastructure/screenplay/actor.js
@@ -1,3 +1,6 @@
+import { attachJson } from '../capture/json.js'
+import PublicRegisterPage from '../pages/public.register.page.js'
+
 export default class Actor {
   const
   constructor(name) {
@@ -17,9 +20,34 @@ export default class Actor {
 
   remembers(key, value) {
     this.memory[key] = value
+    attachJson(this.toJson(), `actor-memory-changed-${key}.json`)
   }
 
   recalls(key) {
     return this.memory[key]
+  }
+
+  toJson() {
+    const memoryWithDescriptions = { ...this.memory }
+
+    if ('publicRegisterChoice' in memoryWithDescriptions) {
+      switch (memoryWithDescriptions.publicRegisterChoice) {
+        case PublicRegisterPage.withhold:
+          memoryWithDescriptions.publicRegisterChoice =
+            'Withhold information from the public register'
+          break
+        case PublicRegisterPage.consent:
+          memoryWithDescriptions.publicRegisterChoice =
+            'Allow information to be added to the public register'
+          break
+        default:
+          break
+      }
+    }
+
+    return {
+      name: this.name,
+      memory: memoryWithDescriptions
+    }
   }
 }
