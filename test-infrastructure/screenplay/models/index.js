63db3d575b759188a8a820a041b493201e1a38f0
Daniel Webb <daniel.webb@esynergy.co.uk>
2025-05-25 18:26:09 +0100

    deleted claude's over-engineering

--- test-infrastructure/screenplay/models/index.js
+++ test-infrastructure/screenplay/models/index.js
@@ -1,26 +1,21 @@
 import ExemptionModel from './exemption.model.js'
 import MarineProjectModel from './marine.project.model.js'
 import PublicRegisterModel from './public.register.model.js'
-import SiteDetailsModel from './site.details.model.js'
 
 export { default as ExemptionData } from './exemption.data.js'
 export { default as ExemptionModel } from './exemption.model.js'
 export { default as MarineProjectModel } from './marine.project.model.js'
 export { default as MemoryFormatter } from './memory.formatter.js'
 export { default as PublicRegisterModel } from './public.register.model.js'
-export { default as SiteDetailsModel } from './site.details.model.js'
 
 export const TestData = {
   MarineProject: MarineProjectModel,
   PublicRegister: PublicRegisterModel,
-  SiteDetails: SiteDetailsModel,
   Exemption: ExemptionModel
 }
 
 export const generateTestData = {
-  projectName: () => MarineProjectModel.generateValidProjectName(),
+  projectName: () => MarineProjectModel.generateProjectName(),
   publicRegister: (withhold = false) =>
-    PublicRegisterModel.generatePublicRegisterData({ withhold }),
-  siteDetails: (region = null) =>
-    SiteDetailsModel.generateSiteDetails({ region })
+    PublicRegisterModel.generatePublicRegisterData({ withhold })
 }
