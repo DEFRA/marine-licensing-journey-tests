1e6d079f0ee517d2b8d857d6b29930b645c5c106
Daniel Webb <daniel.webb@esynergy.co.uk>
2025-05-25 16:50:13 +0100

    refactored to reduce code duplication

--- test-infrastructure/screenplay/factories/apply.for.exemption.js
+++ test-infrastructure/screenplay/factories/apply.for.exemption.js
@@ -5,120 +5,90 @@ export default class ApplyForExemption {
     this.data = data
   }
 
-  static now() {
-    return new ApplyForExemption({
+  static createExemption(overrides = {}) {
+    const defaults = {
       projectName: MarineProjectModel.generateProjectName(),
-      publicRegister: PublicRegisterModel.generatePublicRegisterData(),
+      publicRegister: null,
       projectNameTaskCompleted: false,
       publicRegisterTaskCompleted: false
+    }
+    return new ApplyForExemption({ ...defaults, ...overrides })
+  }
+
+  static now() {
+    return this.createExemption({
+      publicRegister: PublicRegisterModel.generatePublicRegisterData()
     })
   }
 
   static withValidProjectName() {
-    return new ApplyForExemption({
-      projectName: MarineProjectModel.generateProjectName(),
-      publicRegister: null,
-      projectNameTaskCompleted: false,
-      publicRegisterTaskCompleted: false
-    })
+    return this.createExemption()
   }
 
   static withProjectName(projectName) {
-    return new ApplyForExemption({
-      projectName,
-      publicRegister: null,
-      projectNameTaskCompleted: false,
-      publicRegisterTaskCompleted: false
-    })
+    return this.createExemption({ projectName })
   }
 
   static withEmptyProjectName() {
-    return new ApplyForExemption({
-      projectName: '',
-      publicRegister: null,
-      projectNameTaskCompleted: false,
-      publicRegisterTaskCompleted: false
-    })
+    return this.createExemption({ projectName: '' })
   }
 
   static withProjectNameThatIsTooLong() {
-    return new ApplyForExemption({
-      projectName: MarineProjectModel.generateOversizedProjectName(),
-      publicRegister: null,
-      projectNameTaskCompleted: false,
-      publicRegisterTaskCompleted: false
+    return this.createExemption({
+      projectName: MarineProjectModel.generateOversizedProjectName()
     })
   }
 
   static withConsentToPublicRegister() {
-    return new ApplyForExemption({
-      projectName: MarineProjectModel.generateProjectName(),
-      publicRegister: {
-        consent: '#consent-2'
-      },
-      projectNameTaskCompleted: false,
-      publicRegisterTaskCompleted: false
+    return this.createExemption({
+      publicRegister: { consent: '#consent-2' }
     })
   }
 
   static withWithholdFromPublicRegister() {
-    return new ApplyForExemption({
-      projectName: MarineProjectModel.generateProjectName(),
+    return this.createExemption({
       publicRegister: {
         consent: '#consent',
         reason: PublicRegisterModel.generateWithholdingReason()
-      },
-      projectNameTaskCompleted: false,
-      publicRegisterTaskCompleted: false
+      }
     })
   }
 
   static withWithholdFromPublicRegisterButNoReason() {
-    return new ApplyForExemption({
-      projectName: MarineProjectModel.generateProjectName(),
+    return this.createExemption({
       publicRegister: {
         consent: '#consent',
         reason: ''
-      },
-      projectNameTaskCompleted: false,
-      publicRegisterTaskCompleted: false
+      }
     })
   }
 
   static withWithholdReasonThatIsTooLong() {
-    return new ApplyForExemption({
-      projectName: MarineProjectModel.generateProjectName(),
+    return this.createExemption({
       publicRegister: {
         consent: '#consent',
         reason: PublicRegisterModel.generateOversizedReason()
-      },
-      projectNameTaskCompleted: false,
-      publicRegisterTaskCompleted: false
+      }
     })
   }
 
   static withNoPublicRegisterChoice() {
-    return new ApplyForExemption({
-      projectName: MarineProjectModel.generateProjectName(),
+    return this.createExemption({
       publicRegister: {
         consent: null,
         reason: null
       },
-      projectNameTaskCompleted: true,
-      publicRegisterTaskCompleted: false
+      projectNameTaskCompleted: true
     })
   }
 
   static withTemporaryChanges() {
-    return new ApplyForExemption({
-      projectName: MarineProjectModel.generateProjectName(),
+    return this.createExemption({
       publicRegister: {
         consent: 'yes',
         reason: 'This will not be saved'
       },
-      isTemporary: true,
-      projectNameTaskCompleted: false,
-      publicRegisterTaskCompleted: false
+      isTemporary: true
     })
   }
 
