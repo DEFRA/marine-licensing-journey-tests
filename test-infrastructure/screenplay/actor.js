96d19743a9649e8f7de819068ea07101a7249ad4
Daniel Webb <daniel.webb@esynergy.co.uk>
2025-05-29 16:54:29 +0100

    simplifying actor memory patterns

--- test-infrastructure/screenplay/actor.js
+++ test-infrastructure/screenplay/actor.js
@@ -6,7 +6,6 @@ export default class Actor {
   constructor(name) {
     this.name = name
     this.memory = {}
-    this.optionalKeys = ['publicRegisterWithholdReason']
   }
 
   can(ability) {
@@ -48,33 +47,9 @@ export default class Actor {
   }
 
   recalls(key) {
-    if (this.optionalKeys.includes(key)) {
-      return this.memory[key] || ''
-    }
-
     const errorMessage = `Actor '${this.name}' tried to recall '${key}' but it wasn't in memory`
     assert.property(this.memory, key, errorMessage)
     return this.memory[key]
-  }
-
-  recallsOptional(key, defaultValue = '') {
-    return key in this.memory ? this.memory[key] : defaultValue
-  }
-
-  forgets(key) {
-    if (this.optionalKeys.includes(key)) {
-      if (key in this.memory) {
-        delete this.memory[key]
-        attachJson(this.toJson(), `actor-memory-removed-${key}.json`)
-      }
-      return
-    }
-
-    const errorMessage = `Actor '${this.name}' tried to forget '${key}' but it wasn't in memory`
-    assert.property(this.memory, key, errorMessage)
-
-    delete this.memory[key]
-    attachJson(this.toJson(), `actor-memory-removed-${key}.json`)
   }
 
   hasMemoryOf(key) {
