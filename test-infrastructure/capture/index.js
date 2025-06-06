71d4fa920d298583c9c0c1259c4e9e0f97d936fc
Daniel Webb <daniel.webb@esynergy.co.uk>
2025-06-06 13:26:14 +0100

    add feature descriptions to the allure report

--- test-infrastructure/capture/index.js
+++ test-infrastructure/capture/index.js
@@ -1,2 +1,3 @@
-export { attachJson, attachRichFeatureContext } from './json.js'
+export { attachRichFeatureContext } from './feature-context.js'
+export { attachJson } from './json.js'
 export { takeScreenshot } from './screenshot.js'
