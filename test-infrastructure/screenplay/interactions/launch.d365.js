eea31dd6cf2f710e2283079dd069cd5acfabf855
Daniel Webb <daniel.webb@esynergy.co.uk>
2025-07-28 18:57:07 +0100

    handle unnecessary sign in pop up

--- test-infrastructure/screenplay/interactions/launch.d365.js
+++ test-infrastructure/screenplay/interactions/launch.d365.js
@@ -7,11 +7,14 @@ export default class LaunchD365 extends Task {
 
   async performAs(actor) {
     const browseD365 = actor.abilityTo('BrowseD365')
-
     if (!browseD365) {
       throw new Error('Actor must have BrowseD365 ability to launch D365')
     }
     await browseD365.navigateToUrl(process.env.D365_URL)
+    const page = await browseD365.launch()
     await browseD365.clickByRole('button', 'Sign In')
+    page.on('popup', async (popup) => {
+      await popup.close()
+    })
   }
 }
