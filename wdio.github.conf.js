110676c2413755f758b7d374a9ec3f77a10a7284
Daniel Webb <daniel.webb@esynergy.co.uk>
2025-09-05 15:05:47 +0100

    increase number of tests run in github actions

--- wdio.github.conf.js
+++ wdio.github.conf.js
@@ -5,7 +5,7 @@ import {
 } from './test-infrastructure/capture/index.js'
 
 const getTags = () => {
-  return '@smoke'
+  return 'not @wip and not @bug and not @local-only and not @d365 and not @real-defra-id and not @fivium and not @not-github-actions'
 }
 
 export const config = {
