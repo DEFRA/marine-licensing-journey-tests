26499a6e4c6d074d64cdac06ced3012a7e0ae8a1
Daniel Webb <daniel.webb@esynergy.co.uk>
2025-09-10 14:33:25 +0100

    remove unnecessary locator

--- test-infrastructure/pages/footer.page.js
+++ test-infrastructure/pages/footer.page.js
@@ -1,8 +1,6 @@
 export default class FooterPage {
   static locators = {
     privacyLink: '//footer//a[normalize-space(text())="Privacy"]',
-    cookiesLink: '//footer//a[normalize-space(text())="Cookies"]',
-    accessibilityStatementLink:
-      '//footer//a[normalize-space(text())="Accessibility statement"]'
+    cookiesLink: '//footer//a[normalize-space(text())="Cookies"]'
   }
 }
