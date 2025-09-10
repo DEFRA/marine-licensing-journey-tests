df75ad2c88a0bbb9f119361bd7f3927154b3b2e8
Daniel Webb <daniel.webb@esynergy.co.uk>
2025-09-10 14:41:53 +0100

    extract date formatter to a helper

--- test-infrastructure/helpers/index.js
+++ test-infrastructure/helpers/index.js
@@ -2,4 +2,3 @@ export { default as CoordinateFiles } from './coordinate-files.js'
 export * from './date-formatter.js'
 export { DefraIdStubUserManager } from './defra-id-stub-user-manager.js'
 export { constructIatUrl } from './iat-url-builder.js'
-
