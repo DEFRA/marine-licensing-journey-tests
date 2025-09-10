c69f9f4c091ac71098855681c87ec99cada304db
Daniel Webb <daniel.webb@esynergy.co.uk>
2025-09-10 14:09:35 +0100

    scroll into view

--- test-infrastructure/screenplay/interactions/click.privacy.policy.link.js
+++ test-infrastructure/screenplay/interactions/click.privacy.policy.link.js
@@ -9,10 +9,6 @@ export default class ClickPrivacyPolicyLink extends Task {
   async performAs(actor) {
     const browseTheWeb = actor.ability
     await browseTheWeb.isDisplayed(FooterPage.locators.privacyLink)
-    const element = await browseTheWeb.getElement(
-      FooterPage.locators.privacyLink
-    )
-    await element.scrollIntoView()
     await browseTheWeb.click(FooterPage.locators.privacyLink)
   }
 }
