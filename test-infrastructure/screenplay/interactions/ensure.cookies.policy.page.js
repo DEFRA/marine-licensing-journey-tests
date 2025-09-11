7c7ef5b84ce5e673a0ff608ff64738b7f8685cbd
Daniel Webb <daniel.webb@esynergy.co.uk>
2025-09-11 17:01:58 +0100

    implement first test

--- test-infrastructure/screenplay/interactions/ensure.cookies.policy.page.js
+++ test-infrastructure/screenplay/interactions/ensure.cookies.policy.page.js
@@ -3,21 +3,21 @@ import Task from '../base/task.js'
 import EnsurePageHeading from './ensure.page.heading.js'
 
 export default class EnsureCookiesPolicyPage extends Task {
-    static isDisplayed() {
-        return new EnsureCookiesPolicyPage()
-    }
+  static isDisplayed() {
+    return new EnsureCookiesPolicyPage()
+  }
 
-    async performAs(actor) {
-        const browseTheWeb = actor.ability
-        const currentUrl = await browseTheWeb.browser.getUrl()
+  async performAs(actor) {
+    const browseTheWeb = actor.ability
+    const currentUrl = await browseTheWeb.browser.getUrl()
 
-        expect(
-            currentUrl,
-            'The cookies policy page should be displayed'
-        ).to.include('/help/cookies')
+    expect(
+      currentUrl,
+      'The cookies policy page should be displayed'
+    ).to.include('/help/cookies')
 
-        await actor.attemptsTo(
-            EnsurePageHeading.is('Cookies on Get permission for marine work')
-        )
-    }
+    await actor.attemptsTo(
+      EnsurePageHeading.is('Cookies on Get permission for marine work')
+    )
+  }
 }
