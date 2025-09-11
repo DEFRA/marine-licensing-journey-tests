e3fcce67359aaee9bc2834fc3e7fca182840a754
Daniel Webb <daniel.webb@esynergy.co.uk>
2025-09-11 18:12:22 +0100

    implement the next test

--- test-infrastructure/screenplay/interactions/ensure.cookies.radio.button.selected.js
+++ test-infrastructure/screenplay/interactions/ensure.cookies.radio.button.selected.js
@@ -1,23 +1,23 @@
 import Task from '../base/task.js'
 
 export default class EnsureCookiesRadioButtonSelected extends Task {
-    static is(radioOption) {
-        return new EnsureCookiesRadioButtonSelected(radioOption)
-    }
+  static is(radioOption) {
+    return new EnsureCookiesRadioButtonSelected(radioOption)
+  }
 
-    constructor(radioOption) {
-        super()
-        this.radioOption = radioOption
-    }
+  constructor(radioOption) {
+    super()
+    this.radioOption = radioOption
+  }
 
-    async performAs(actor) {
-        const browseTheWeb = actor.ability
+  async performAs(actor) {
+    const browseTheWeb = actor.ability
 
-        const radioSelector =
-            this.radioOption === 'Yes'
-                ? 'input[name="analytics"][value="yes"]'
-                : 'input[name="analytics"][value="no"]'
+    const radioSelector =
+      this.radioOption === 'Yes'
+        ? 'input[name="analytics"][value="yes"]'
+        : 'input[name="analytics"][value="no"]'
 
-        await browseTheWeb.isSelected(radioSelector)
-    }
+    await browseTheWeb.isSelected(radioSelector)
+  }
 }
