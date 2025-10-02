294cd4ed214ee9676ed56b985eab033f5b193fbe
Daniel Webb <daniel.webb@esynergy.co.uk>
2025-10-02 17:23:13 +0100

    tidy up

--- test/steps/redirection.to.login.steps.js
+++ test/steps/redirection.to.login.steps.js
@@ -1,9 +1,10 @@
 import { Given, Then, When } from '@cucumber/cucumber'
-import DashboardPage from '~/test-infrastructure/pages/dashboard.page.js'
 import EnsureDashboardPage from '~/test-infrastructure/screenplay/interactions/ensure.dashboard.page.js'
 import EnsureViewDetailsPage from '~/test-infrastructure/screenplay/interactions/ensure.view.details.page.js'
+import NavigateToViewDetailsLink from '~/test-infrastructure/screenplay/interactions/navigate.to.view.details.link.js'
 import SignIn from '~/test-infrastructure/screenplay/interactions/sign.in.js'
 import SignOut from '~/test-infrastructure/screenplay/interactions/sign.out.js'
+import NavigateToDashboard from '~/test-infrastructure/screenplay/tasks/navigate.to.dashboard.js'
 
 Given('the user is logged out', async function () {
   await this.actor.attemptsTo(SignOut.now())
@@ -12,14 +13,7 @@ Given('the user is logged out', async function () {
 When(
   'the user clicks on the link to View Details page and logs in',
   async function () {
-    const browseTheWeb = this.actor.ability
-    const viewDetailsLink = this.actor.recalls('viewDetailsLink')
-    if (!viewDetailsLink || typeof viewDetailsLink !== 'string') {
-      throw new Error(
-        `Expected viewDetailsLink to be a string, but got: ${typeof viewDetailsLink}`
-      )
-    }
-    await browseTheWeb.navigateTo(viewDetailsLink)
+    await this.actor.attemptsTo(NavigateToViewDetailsLink.now())
     await this.actor.attemptsTo(SignIn.now())
   }
 )
@@ -31,8 +25,7 @@ Then('subsequently redirected to the View Details page', async function () {
 When(
   'the user clicks on the link to the Dashboard and logs in',
   async function () {
-    const browseTheWeb = this.actor.ability
-    await browseTheWeb.navigateTo(DashboardPage.url)
+    await this.actor.attemptsTo(NavigateToDashboard.now())
     await this.actor.attemptsTo(SignIn.now())
   }
 )
