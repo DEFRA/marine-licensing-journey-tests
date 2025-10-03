08db5aff84398f81e18b6ecb65079c3794e0972d
Daniel Webb <daniel.webb@esynergy.co.uk>
2025-10-03 10:55:58 +0100

    add additional check for organisation name in header

--- test-infrastructure/screenplay/interactions/ensure.organisation.name.js
+++ test-infrastructure/screenplay/interactions/ensure.organisation.name.js
@@ -2,17 +2,17 @@ import CommonElementsPage from '~/test-infrastructure/pages/common.elements.page
 import Task from '../base/task.js'
 
 export default class EnsureOrganisationName extends Task {
-    static isDisplayed() {
-        return new EnsureOrganisationName()
-    }
+  static isDisplayed() {
+    return new EnsureOrganisationName()
+  }
 
-    async performAs(actor) {
-        const testUser = actor.recalls('testUser')
-        const expectedOrganisationName = testUser.relationships[0].organisationName
+  async performAs(actor) {
+    const testUser = actor.recalls('testUser')
+    const expectedOrganisationName = testUser.relationships[0].organisationName
 
-        await actor.ability.expectElementToContainText(
-            CommonElementsPage.organisationName,
-            expectedOrganisationName
-        )
-    }
+    await actor.ability.expectElementToContainText(
+      CommonElementsPage.organisationName,
+      expectedOrganisationName
+    )
+  }
 }
