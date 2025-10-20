f3a9ee90927ec4c069c591bace1ea6f3a206e2ac
Daniel Webb <daniel.webb@esynergy.co.uk>
2025-10-20 13:53:43 +0100

    wip

--- test-infrastructure/pages/project.name.page.js
+++ test-infrastructure/pages/project.name.page.js
@@ -2,5 +2,5 @@ export default class ProjectNamePage {
   static url = '/'
   static projectNameInput = '#projectName'
   static projectNameError = '#projectName-error'
-  static saveAndContinue = 'button*=Save and continue'
+  static saveAndContinue = 'button[type="submit"]'
 }
