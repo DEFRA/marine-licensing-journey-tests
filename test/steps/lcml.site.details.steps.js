8db45c5107bcaa40fc1321132805d2de32ed69a0
Kamesh Raj Rajendran <krrajendran@ip-192-168-1-195.eu-west-2.compute.internal>
2026-03-31 10:35:17 +0100

    ML-1242 Extract shared LCML helpers to reduce code duplication
    - Extract loginAndStartApplication and completeSpecialLegalPowers to test/support/lcml-helpers.js
    - Remove duplicated login/setup code from lcml.apply.steps.js and lcml.site.details.steps.js
    - Both step files now import from shared helper

--- test/steps/lcml.site.details.steps.js
+++ test/steps/lcml.site.details.steps.js
@@ -1,59 +1,13 @@
 import { Given, When, Then } from '@cucumber/cucumber'
 import { expect } from '@playwright/test'
-import { faker } from '@faker-js/faker'
-import { getConfig } from '../support/config.js'
 import {
-  registerTestUser,
-  loginAsTestUser,
-  acceptCookies
-} from '../support/auth.js'
+  loginAndStartApplication,
+  completeSpecialLegalPowers
+} from '../support/lcml-helpers.js'
 
 async function loginAndReachTaskList(world) {
-  const config = getConfig()
-
-  if (!config.isRealDefraId && !world.testUser) {
-    world.testUser = await registerTestUser(config.defraIdUrl, {
-      userType: 'employee'
-    })
-  }
-
-  const projectName = `${faker.location.city()} ${faker.company.buzzNoun()} - Phase ${faker.number.int({ min: 1, max: 9 })} ${faker.number.int({ min: 1000, max: 9999 })}`
-  world.data = { projectName }
-
-  await world.page.goto(new URL('/home', config.baseURL).toString())
-
-  if (!config.isRealDefraId) {
-    await loginAsTestUser(world.page, world.testUser)
-  }
-
-  await acceptCookies(world.page)
-
-  // Confirm user type if on confirm page
-  if (world.page.url().includes('/confirm-')) {
-    await world.page.locator('#confirmEmployee').click()
-    await world.page.locator('button[type="submit"]').click()
-    await world.page.waitForLoadState('load')
-  }
-
-  // Click "Apply for a marine licence" on home page
-  await world.page
-    .getByRole('link', { name: 'Apply for a marine licence' })
-    .click()
-  await world.page.waitForLoadState('load')
-
-  await world.page.locator('#projectName').fill(projectName)
-  await world.page.locator('button:has-text("Save and continue")').click()
-  await world.page.waitForLoadState('load')
-
-  // Complete special legal powers if present
-  const slpLink = world.page.locator('a:has-text("Special legal powers")')
-  if (await slpLink.isVisible({ timeout: 3000 }).catch(() => false)) {
-    await slpLink.click()
-    await world.page.waitForLoadState('load')
-    await world.page.locator('#agree-2').click()
-    await world.page.locator('button:has-text("Save and continue")').click()
-    await world.page.waitForLoadState('load')
-  }
+  await loginAndStartApplication(world, 'organisation')
+  await completeSpecialLegalPowers(world.page, 'No')
 }
 
 Given('an organisation user is on the site details page', async function () {
