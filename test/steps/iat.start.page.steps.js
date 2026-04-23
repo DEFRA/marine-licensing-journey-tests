a855f6caffc4284706db60db7782e0dc6fcba94e
Kamesh Rajendran <kamesh.rajendran@equalexperts.com>
2026-04-23 14:16:39 +0100

    ML-1163 Add IAT journey tests for start page and question pages (#271)
    - Add IAT feature with start page, question navigation, back nav and full flow scenarios
    - Add iat profile to cucumber.mjs
    - Add ENABLE_SELF_SERVICE env var to compose.yml and compose.common.yml
    
    Co-authored-by: Kamesh Raj Rajendran <krrajendran@ip-192-168-1-217.eu-west-2.compute.internal>
    Co-authored-by: Vasudev Ganesan <vasudev.ganesan@esynergy.co.uk>

---
+++ test/steps/iat.start.page.steps.js
@@ -0,0 +1,72 @@
+import { Given, When, Then } from '@cucumber/cucumber'
+import { expect } from '@playwright/test'
+import { getConfig } from '../support/config.js'
+
+const IAT_START_PATH = '/journey/self-service/start'
+
+const EXPECTED_LINKS = [
+  {
+    text: 'jurisdiction',
+    href: 'https://www.gov.uk/guidance/marine-licensing-definitions#jurisdiction'
+  },
+  {
+    text: 'exemptions',
+    href: 'https://www.gov.uk/guidance/do-i-need-a-marine-licence#exemptions'
+  },
+  {
+    text: 'self-service marine licensing',
+    href: 'https://www.gov.uk/guidance/do-i-need-a-marine-licence#self-service'
+  },
+  {
+    text: 'guidance',
+    href: 'https://www.gov.uk/guidance/do-i-need-a-marine-licence'
+  }
+]
+
+Given('a user navigates to the IAT start page', async function () {
+  const config = getConfig()
+  await this.page.goto(new URL(IAT_START_PATH, config.baseURL).toString())
+  await this.page.waitForLoadState('load')
+})
+
+Given('a user is on the IAT start page', async function () {
+  const config = getConfig()
+  await this.page.goto(new URL(IAT_START_PATH, config.baseURL).toString())
+  await this.page.waitForLoadState('load')
+})
+
+When('the user views the start page', async function () {
+  // Already on start page from Given step
+})
+
+Then('the heading {string} is displayed', async function (expectedHeading) {
+  await expect(this.page.locator('h1').first()).toContainText(expectedHeading, {
+    timeout: 30_000
+  })
+})
+
+Then(
+  'the start page contains guidance links that open in a new tab',
+  async function () {
+    for (const link of EXPECTED_LINKS) {
+      const locator = this.page.locator(`main a[href="${link.href}"]`)
+      await expect(locator).toBeVisible({ timeout: 30_000 })
+      await expect(locator).toHaveAttribute('target', '_blank')
+    }
+  }
+)
+
+Then('there are no links in the page header', async function () {
+  const headerNavLinks = this.page.locator(
+    '.govuk-service-navigation__list a, .govuk-header__navigation a'
+  )
+  await expect(headerNavLinks).toHaveCount(0)
+})
+
+Then('a {string} button is displayed', async function (buttonText) {
+  await expect(
+    this.page.locator(
+      `button:has-text("${buttonText}"), a.govuk-button:has-text("${buttonText}")`
+    )
+  ).toBeVisible({ timeout: 30_000 })
+})
