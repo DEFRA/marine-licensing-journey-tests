d672c4cb1ab128fa5eb1be984ec21c83b6e08e95
Kamesh Rajendran <kamesh.rajendran@equalexperts.com>
2026-06-08 14:23:02 +0100

    ML-1326 remove Before you start WFD page display scenario

--- test/features/lcml/lcml.water.framework.directive.feature
+++ test/features/lcml/lcml.water.framework.directive.feature
@@ -11,14 +11,6 @@ Feature: LCML: Water Framework Directive
     Then the "Water Framework Directive" section heading is displayed on the task list
     And the "Water Framework Directive assessment" task is displayed with status "Not yet started"
 
-  @issue=ML-1312
-  Scenario: Selecting the task displays the Before you start WFD page
-    Given an organisation user has started a marine licence application
-    When the user opens the Water Framework Directive assessment task
-    Then the Before you start WFD page is displayed with the project name in the caption
-    And the "Help with excluded activities" section is initially collapsed
-    And the guidance link opens the Water Framework Directive guidance on gov.uk in a new tab
-
   @issue=ML-1326
   Scenario: Selecting "No" on the One nautical mile page completes the WFD assessment task
     Given an organisation user is on the One nautical mile WFD page
--- test/steps/lcml.water.framework.directive.steps.js
+++ test/steps/lcml.water.framework.directive.steps.js
@@ -3,11 +3,8 @@ import { expect } from '@playwright/test'
 import { loginAndStartApplication } from '../support/lcml-helpers.js'
 
 const TASK_LINK = 'Water Framework Directive assessment'
-const PAGE_PATH = '/marine-licence/water-framework-directive-before-you-start'
 const NAUTICAL_MILE_PATH =
   '/marine-licence/water-framework-directive-nautical-mile'
-const GUIDANCE_URL =
-  'https://www.gov.uk/guidance/water-framework-directive-assessment-estuarine-and-coastal-waters'
 const NAUTICAL_MILE_RADIO_IDS = { Yes: '#nauticalMile', No: '#nauticalMile-2' }
 
 async function openWfdTask(page) {
@@ -15,67 +12,12 @@ async function openWfdTask(page) {
   await page.waitForLoadState('load')
 }
 
-async function expectOnWfdPage(page) {
-  await expect(page).toHaveURL(new RegExp(PAGE_PATH), { timeout: 30_000 })
-  await expect(page.locator('main h1')).toContainText(
-    'Water Framework Directive',
-    {
-      timeout: 30_000
-    }
-  )
-  await expect(
-    page.getByRole('heading', { name: 'Before you start', level: 2 })
-  ).toBeVisible({ timeout: 30_000 })
-}
-
-When(
-  'the user opens the Water Framework Directive assessment task',
-  async function () {
-    await openWfdTask(this.page)
-  }
-)
-
 Then(
   'the {string} section heading is displayed on the task list',
   async function (heading) {
     await expect(
       this.page.getByRole('heading', { name: heading, level: 2, exact: true })
     ).toBeVisible({ timeout: 30_000 })
-  }
-)
-
-Then(
-  'the Before you start WFD page is displayed with the project name in the caption',
-  async function () {
-    await expectOnWfdPage(this.page)
-    await expect(
-      this.page.locator('main .govuk-caption-l').first()
-    ).toContainText(this.data.projectName, { timeout: 30_000 })
-  }
-)
-
-Then(
-  'the {string} section is initially collapsed',
-  async function (summaryText) {
-    const details = this.page.locator(
-      `details.govuk-details:has(summary:has-text("${summaryText}"))`
-    )
-    await expect(details).toBeVisible({ timeout: 30_000 })
-    expect(await details.evaluate((el) => el.open)).toBe(false)
-  }
-)
-
-Then(
-  'the guidance link opens the Water Framework Directive guidance on gov.uk in a new tab',
-  async function () {
-    const link = this.page.locator(
-      'main a:has-text("Read the guidance Water Framework Directive")'
-    )
-    await expect(link).toBeVisible({ timeout: 30_000 })
-    await expect(link).toHaveAttribute('href', GUIDANCE_URL, {
-      timeout: 30_000
-    })
-    await expect(link).toHaveAttribute('target', '_blank', { timeout: 30_000 })
   }
 )
 
